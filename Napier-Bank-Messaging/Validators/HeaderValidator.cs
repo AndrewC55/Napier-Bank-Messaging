53082799e364be6fc5fdad956df5ffa5028416b4
AndrewC55 <AndrewC55>
2021-08-17 14:04:40 +0100

    Added header length and message type valid functions

--- Napier-Bank-Messaging/Validators/HeaderValidator.cs
+++ Napier-Bank-Messaging/Validators/HeaderValidator.cs
@@ -8,5 +8,11 @@ namespace Napier_Bank_Messaging.Validators
 {
     class HeaderValidator
     {
+        const int HeaderLength = 10;
+        const string Sms       = "S";
+        const string EMAIL     = "E";
+        const string Tweet     = "T";
+
+        static Boolean
     }
 }
