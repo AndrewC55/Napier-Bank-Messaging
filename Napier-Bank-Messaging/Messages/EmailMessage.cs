18c8c91d0947b85dc1a20ab2226f546e94517f85
AndrewC55 <AndrewC55>
2021-08-17 22:17:01 +0100

    Added check subject method and made regex for email

--- Napier-Bank-Messaging/Messages/EmailMessage.cs
+++ Napier-Bank-Messaging/Messages/EmailMessage.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Napier_Bank_Messaging.Messages
 {
-    class EmailMessage
+    class EmailMessage : Message
     {
+        public override void Sanatise(string body)
+        {
+        }
+
+        public override bool Format(string body)
+        {
+            List<string> listBody = body.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+            return IsSenderCorrect(listBody[0]) && IsSubjectCorrect(listBody[1]) && IsCharacterLengthCorrect(listBody[2]);
+        }
+
+        private bool IsSenderCorrect(string body)
+        {
+            bool isValid = true;
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(body);
+
+            return match.Success;
+        }
+
+        private bool IsSubjectCorrect(string body)
+        {
+            bool isValid = true;
+
+            if (body.Length > 20)
+            {
+                return !isValid;
+            }
+
+            return isValid;
+        }
+
+        private static bool IsCharacterLengthCorrect(string body)
+        {
+            bool isValid = true;
+
+            if (body.Length > 1028)
+            {
+                return !isValid;
+            }
+
+            return isValid;
+        }
     }
 }
