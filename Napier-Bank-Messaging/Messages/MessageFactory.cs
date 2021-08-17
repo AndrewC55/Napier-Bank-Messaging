d95c93cb93dd68f3788dd53348d183beb6f6e444
AndrewC55 <AndrewC55>
2021-08-17 18:09:21 +0100

    Create Message factory

---
+++ Napier-Bank-Messaging/Messages/MessageFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napier_Bank_Messaging.Messages
+{
+    class MessageFactory
+    {
+        private string _messageType;
+
+        public string MessageType
+        {
+            get => _messageType;
+            set => _messageType = value;
+        }
+
+        public Message Factory()
+        {
+            return new Message();
+        }
+    }
+}
