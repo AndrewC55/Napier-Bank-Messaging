b9d0606db16c57dd934a89ce620dacb420766bee
AndrewC55 <AndrewC55>
2021-08-17 16:19:38 +0100

    Added Message class

---
+++ Napier-Bank-Messaging/Messages/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napier_Bank_Messaging.Messages
+{
+    public abstract class Message
+    {
+        public string _messageHeader, _messageBody;
+
+        public string MessageHeader
+        {
+            get => _messageHeader;
+            set => _messageHeader = value;
+        }
+
+        public string MessageBody
+        {
+            get => _messageBody;
+            set => _messageBody = value;
+        }
+    }
+}
