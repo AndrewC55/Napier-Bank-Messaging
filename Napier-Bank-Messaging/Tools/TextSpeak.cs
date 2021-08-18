db1b2190f8b55c75701bbe32e22dfbec3d1b9039
AndrewC55 <AndrewC55>
2021-08-18 10:28:05 +0100

    Added TextSpeak class and made sanitiser class

---
+++ Napier-Bank-Messaging/Tools/TextSpeak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napier_Bank_Messaging.Tools
+{
+    public class TextSpeak
+    {
+        private string _abbreviation, _phrase;
+
+        public string Abbreviation
+        {
+            get => _abbreviation;
+            set => _abbreviation = value;
+        }
+
+        public string Phrase
+        {
+            get => _phrase;
+            set => _phrase = value;
+        }
+    }
+}
---
+++ Napier-Bank-Messaging/Tools/TextSpeakSanitiser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Napier_Bank_Messaging.Tools
+{
+    class TextSpeakSanitiser
+    {
+        public static List<TextSpeak> GetTextSpeakerValues()
+        {
+            string filePath = "";
+            string[] values = File.ReadAllLines(filePath);
+            List<TextSpeak> allValues = new List<TextSpeak>();
+            
+            foreach (string value in values)
+            {
+                string[] row = value.Split(",");
+                TextSpeak textSpeak = new TextSpeak();
+                textSpeak.Abbreviation = row[0];
+                textSpeak.Phrase = row[1];
+                allValues.Add(textSpeak);
+            }
+
+            return allValues;
+        }
+    }
+}
